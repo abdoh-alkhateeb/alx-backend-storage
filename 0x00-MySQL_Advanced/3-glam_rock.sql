52ce8be64a635902b0fe14bdb33a308e2a627990
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-10-29 21:30:12 +0000

    a

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam rock as their main style ranked by their longevity.
 SELECT band_name, (COALESCE(split, 2022) - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
