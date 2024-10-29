419df77a83b98b75639b8b9bffce5aed43d7734a
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-10-29 21:55:48 +0000

    s

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,8 @@
+-- Creates a trigger that decreases the quantity of an item after adding a new order.
+CREATE TRIGGER after_order_insert AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE name=NEW.item_name;
+ENG;
